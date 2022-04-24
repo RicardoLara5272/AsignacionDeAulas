958557f91c0586d327d64ab4eb9b17d45054a3da
evelinviscarra <evelin.viscarra@gmail.com>
2022-04-24 00:02:27 -0400

    header y footer

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+    include('vista/layouts/header.php');
+    $active = "active";
+    
+
+?>
+<div class="container" style="width: 650px;margin-top: 0;">
+	<div class="row">
+		<div class="col-md-12">			
+			<div class="Administrador">
+				<hr>
+				<h5>Seccion Docentes</h5>
+				<hr>
+				<a class="btn btn-success" style="width: 300px;background-color: #45A049;" href="/vista/solicitudes_vista.php">Solicitar Reserva de aula</a>
+				<hr>
+            </div>
+        </div>
+    </div>
+</div>
+				
+
+<?php include('vista/layouts/footer.php') ?>
+    
---
+++ vista/pruebahtml.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formularios</title>
+</head>
+<body>
+    <!-- <table border="1">
+        <caption><strong>Titulo de tabla</strong></caption>
+        <tr>
+            <th>Columna 1</th>
+            <th>Columna 2</th>
+            <th>Columna 3</th>
+        </tr>
+        <tr>
+            <td>Contenido</td>
+            <td colspan="2"></td>
+        </tr>
+    </table> -->
+    <h1>Formulario de registro</h1>
+    <label for="">Nombre completo</label>
+    <input type="text">
+    <br>
+    <label for="">Fecha de nacimiento</label>
+    <input type="date">
+    <br>
+    <label for="">Sexo</label>
+    <input type="radio" name="sexo">Hombre
+    <input type="radio" name="sexo">Mujer
+    <br>
+    <input type="button" value="Enviar">
+
+</body>
+</html>
