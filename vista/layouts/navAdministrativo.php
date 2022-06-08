e69b4ac3fa6e71634cd403f2a2330605c4d7efa3
jhon47578 <jhon475789@gmail.com>
2022-06-07 20:17:25 -0400

    modificacion detalles

--- vista/layouts/navAdministrativo.php
+++ vista/layouts/navAdministrativo.php
@@ -2,30 +2,46 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
     <title>Asignaciones</title>
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300&display=swap" rel="stylesheet">
+
+<!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     
+    <link rel="stylesheet" href="css/estiloSolicitud.css">
 </head>
 <body>
-<header>
-    <h1>Sistema de asignación de aulas</h1>
-</header>
+
+<div class="container-fluid text-white bg-dark">
+    <div class="row">
+        <div class="col-md">
+            <header class="py-3">
+                <h3>Sistema de asignación de aulas</h3>
+            </header>
+        </div>
+    </div>
+</div>
+
 <nav>
     <section id="menu-nav">
         <ul class="menu">
-                <li class="reserva">Solicitudes
-                    <ul class="ul-second">
-                        <li><a class="revisadas" href="/vista/homeAdministrativo.php">Pendientes</a></li>
-                        <li><a class="pendientes" href="/vista/vistaDetRevi.php">Revisadas</a></li>
-                    </ul>
-                </li>
+            <li class="reserva">Mostrar
+                <ul class="ul-second">
+                    <li><a class="Pendientes" href="./vistaDetPend.php">Pendientes</a></li>
+                    <li><a class="Revisadas" href="./vistaDetRevi.php">Revisadas</a></li>
+                </ul>
+            </li>
+            <li>Seguimientos</li>
+            <li>Otros</li>
         </ul>
     </section>
 </nav>
-</body>
-</html>
+
+<div class="container">
+    <div class="ListaSolicitudReserva">
